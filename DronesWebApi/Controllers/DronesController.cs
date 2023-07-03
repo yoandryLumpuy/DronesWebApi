d17b4d857283001e331850fc12c1b006472b47dd
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-03 10:52:35 -0400

    Adding Actions for GET /{droneId}, GET /paginated , GET /available

--- DronesWebApi/Controllers/DronesController.cs
+++ DronesWebApi/Controllers/DronesController.cs
@@ -4,6 +4,10 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using DronesWebApi.Commons.Models;
+using DronesWebApi.Models.Drone.Queries.GetAllAvailableDronesForLoadingQuery;
+using DronesWebApi.Models.Drone.Queries.GetDroneQuery;
+using DronesWebApi.Models.Drone.Queries.GetDronesWithPaginationQuery;
+using DronesWebApi.Models.DroneModel.Queries.GetAllDroneModelsQuery;
 
 namespace DronesWebApi.Controllers
 {
@@ -22,15 +26,43 @@ namespace DronesWebApi.Controllers
             return Ok(Result.Success(result));
         }
 
-        [HttpPost]
+        [HttpGet(template: "{droneId}")]
         [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.UnprocessableEntity)]
         [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.InternalServerError)]
         [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.NotFound)]
-        public async Task<IActionResult> GetAsync(CreateDroneCommand request, CancellationToken cancellation)
+        public async Task<IActionResult> GetAsync([FromRoute] int droneId, CancellationToken cancellation)
         {
-            var result = await Mediator.Send(request, cancellation).ConfigureAwait(false);
+            var result = await Mediator.Send(request: new GetDroneQuery(){ Id = droneId }, cancellation).ConfigureAwait(false);
+
+            return Ok(Result.Success(result));
+        }
+
+        [HttpGet("paginated")]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.UnprocessableEntity)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetWithPaginationAsync([FromQuery] int pageNumber, [FromQuery] int pageSize, CancellationToken cancellation)
+        {
+            var result = await Mediator.Send(
+                request: new GetDronesWithPaginationQuery(){ PageNumber = pageNumber, PageSize = pageSize }, cancellation)
+                .ConfigureAwait(false);
+
+            return Ok(Result.Success(result));
+        }
+
+        [HttpGet("available")]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.UnprocessableEntity)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(Result<>), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetAllAvailableForLoadingAsync(CancellationToken cancellation)
+        {
+            var result = await Mediator.Send(request: new GetAllAvailableDronesForLoadingQuery(), cancellation).ConfigureAwait(false);
 
             return Ok(Result.Success(result));
         }

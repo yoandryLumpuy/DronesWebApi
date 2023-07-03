90fbf1105a53bdb48331b67d839f87b7308c85a0
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-03 10:49:29 -0400

    Adding GetNotLoadedRegisteredMedicationsQuery for obtaining medication item that were registered but not associated with any Done for transportation

---
+++ DronesWebApi/Models/Medication/Queries/GetNotLoadedRegisteredMedicationsQuery/GetNotLoadedRegisteredMedicationsQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using DronesWebApi.Core;
+using DronesWebApi.Models.Medication.Queries.GetMedicationQuery;
+using MediatR;
+
+namespace DronesWebApi.Models.Medication.Queries.GetNotLoadedRegisteredMedicationsQuery
+{
+    public class GetNotLoadedRegisteredMedicationsQuery: IRequest<IEnumerable<MedicationDto>>
+    { }
+
+    public class GetNotLoadedRegisteredMedicationsQueryHandler : IRequestHandler<GetNotLoadedRegisteredMedicationsQuery, IEnumerable<MedicationDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetNotLoadedRegisteredMedicationsQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<IEnumerable<MedicationDto>> Handle(GetNotLoadedRegisteredMedicationsQuery request, CancellationToken cancellationToken)
+        {
+            var entities = _unitOfWork.Medications.GetNotLoaded();
+
+            return Task.FromResult(_mapper.Map<IEnumerable<MedicationDto>>(entities));
+        }
+    }
+}

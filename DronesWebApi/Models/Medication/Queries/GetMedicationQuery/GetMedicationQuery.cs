f354dfb5634b3e2f0aaaaa5f80b5d9a4f4b17749
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-03 10:50:14 -0400

    Adding GetMedicationQuery for searching one medication item by his code

---
+++ DronesWebApi/Models/Medication/Queries/GetMedicationQuery/GetMedicationQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using DronesWebApi.Core;
+using MediatR;
+
+namespace DronesWebApi.Models.Medication.Queries.GetMedicationQuery
+{
+    public class GetMedicationQuery: IRequest<MedicationDto>
+    {
+        public string Code { get; set; }
+    }
+
+    public class GetMedicationQueryHandler : IRequestHandler<GetMedicationQuery, MedicationDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetMedicationQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<MedicationDto> Handle(GetMedicationQuery request, CancellationToken cancellationToken)
+        {
+            var medication = _unitOfWork.Medications.GetWithDronesReferences(request.Code);
+
+            return Task.FromResult(_mapper.Map<MedicationDto>(medication));
+        }
+    }
+}
---
+++ DronesWebApi/Models/Medication/Queries/GetMedicationQuery/GetMedicationQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using DronesWebApi.Core;
+using FluentValidation;
+
+namespace DronesWebApi.Models.Medication.Queries.GetMedicationQuery
+{
+    public class GetMedicationQueryValidator: AbstractValidator<GetMedicationQuery>
+    {
+        public GetMedicationQueryValidator(IUnitOfWork unitOfWork)
+        {
+            RuleFor(medicationQuery => medicationQuery.Code)
+                .Must(code =>
+                {
+                    var medication = unitOfWork.Medications.Get(code);
+
+                    return medication != null;
+
+                }).WithMessage(medicationQuery => $"Medication with code: '{medicationQuery.Code}' was not found");
+        }
+    }
+}

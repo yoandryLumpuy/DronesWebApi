8749cbac5056267b8c3b87c3801b8310ff2339ed
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-03 10:47:04 -0400

    Adding GetLoadedMedicationsQuery for obtaining all loaded medication items for one Drone

---
+++ DronesWebApi/Models/Medication/Queries/GetLoadedMedicationsQuery/GetLoadedMedicationsQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using DronesWebApi.Core;
+using DronesWebApi.Models.Medication.Queries.GetMedicationQuery;
+using MediatR;
+
+namespace DronesWebApi.Models.Medication.Queries.GetLoadedMedicationsQuery
+{
+    public class GetLoadedMedicationsQuery: IRequest<IEnumerable<MedicationDto>>
+    {
+        public int DroneId { get; set; }
+    }
+
+    public class GetLoadedMedicationsQueryHandler : IRequestHandler<GetLoadedMedicationsQuery, IEnumerable<MedicationDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetLoadedMedicationsQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task<IEnumerable<MedicationDto>> Handle(GetLoadedMedicationsQuery request, CancellationToken cancellationToken)
+        {
+            var drone = _unitOfWork.Drones.GetWithLoadedMedications(request.DroneId);
+
+            var resultAsCollection = _mapper.Map<ICollection<MedicationDto>>(drone.LoadedMedications);
+
+            return Task.FromResult((IEnumerable<MedicationDto>)resultAsCollection);
+        }
+    }
+}
---
+++ DronesWebApi/Models/Medication/Queries/GetLoadedMedicationsQuery/GetLoadedMedicationsQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper.Configuration;
+using DronesWebApi.Core;
+using DronesWebApi.Persistence;
+using FluentValidation;
+
+namespace DronesWebApi.Models.Medication.Queries.GetLoadedMedicationsQuery
+{
+    public class GetLoadedMedicationsQueryValidator: AbstractValidator<GetLoadedMedicationsQuery>
+    {
+        public GetLoadedMedicationsQueryValidator(IUnitOfWork unitOfWork)
+        {
+            RuleFor(getLoadedMedicationsQuery => getLoadedMedicationsQuery.DroneId)
+                .Must(droneId =>
+                {
+                    var drone = unitOfWork.Drones.Get(droneId);
+
+                    return drone != null;
+
+                }).WithMessage(getLoadedMedicationsQuery => $"Drone with id: '{getLoadedMedicationsQuery.DroneId}' was not found");
+        }
+    }
+}

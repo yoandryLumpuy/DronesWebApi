6990b1b8627c977832fd76939848c9efdf62383a
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-03 10:25:00 -0400

    Adding GetAllAvailableDronesForLoadingQuery

---
+++ DronesWebApi/Models/Drone/Queries/GetAllAvailableDronesForLoadingQuery/GetAllAvailableDronesForLoadingQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using DronesWebApi.Core;
+using DronesWebApi.Models.Drone.Queries.GetDroneQuery;
+using MediatR;
+
+namespace DronesWebApi.Models.Drone.Queries.GetAllAvailableDronesForLoadingQuery
+{
+    public class GetAllAvailableDronesForLoadingQuery: IRequest<IEnumerable<DroneDto>>
+    { }
+
+    public class GetAllAvailableDronesForLoadingQueryHandler : IRequestHandler<GetAllAvailableDronesForLoadingQuery, IEnumerable<DroneDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllAvailableDronesForLoadingQueryHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<IEnumerable<DroneDto>> Handle(GetAllAvailableDronesForLoadingQuery request, CancellationToken cancellationToken)
+        {
+            var elements = _unitOfWork.Drones.GetAvailableForLoading();
+
+            return Task.FromResult(_mapper.Map<IEnumerable<DroneDto>>(elements));
+        }
+    }
+}

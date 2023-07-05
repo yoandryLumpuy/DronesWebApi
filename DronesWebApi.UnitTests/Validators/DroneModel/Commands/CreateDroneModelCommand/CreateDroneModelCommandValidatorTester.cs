a20e6a5121a853770bab74f630b1dcfcdd611595
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-05 11:42:49 -0400

    Adding tests for DroneModel commands and queries validators

--- DronesWebApi.UnitTests/Validators/DroneModel/Commands/CreateDroneModelCommand/CreateDroneModelCommandValidatorTester.cs
+++ DronesWebApi.UnitTests/Validators/DroneModel/Commands/CreateDroneModelCommand/CreateDroneModelCommandValidatorTester.cs
@@ -1,30 +1,188 @@
-﻿using FluentValidation;
+﻿using System;
+using System.Linq;
+using DronesWebApi.Models.DroneModel.Commands.CreateDroneModelCommand;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Xunit;
 
 namespace DronesWebApi.UnitTests.Validators.DroneModel.Commands.CreateDroneModelCommand
 {
     public class CreateDroneModelCommandValidatorTester
     {
+        private readonly CreateDroneModelCommandValidator _validator;
+
         public CreateDroneModelCommandValidatorTester()
         {
-            //RuleFor(command => command.Name).NotNull().WithMessage($"Name can't be null")
-            //    .NotEmpty().WithMessage($"Name can't be empty")
-            //    .MaximumLength(100).WithMessage($"Maximum length of Name is 100");
+            _validator = new CreateDroneModelCommandValidator();
+        }
 
-            //RuleFor(m => m.CruiserweightInGrams)
-            //    .GreaterThan(0)
-            //    .WithMessage($"Field {nameof(CreateDroneModelCommand.CruiserweightInGrams)} must be greater than zero");
+        [Fact]
+        public void Should_throw_error_when_name_is_null()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                Name = null
+            };
 
-            //RuleFor(m => m.MiddleweightInGrams)
-            //    .GreaterThan(0)
-            //    .WithMessage($"Field {nameof(CreateDroneModelCommand.MiddleweightInGrams)} must be greater than zero");
+            var result = _validator.TestValidate(model);
 
-            //RuleFor(m => m.LightweightInGrams)
-            //    .GreaterThan(0)
-            //    .WithMessage($"Field {nameof(CreateDroneModelCommand.LightweightInGrams)} must be greater than zero");
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
 
-            //RuleFor(m => m.HeavyweightInGrams)
-            //    .GreaterThan(0)
-            //    .WithMessage($"Field {nameof(CreateDroneModelCommand.HeavyweightInGrams)} must be greater than zero");
+        [Fact]
+        public void Should_throw_error_when_name_is_empty()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                Name = " "
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_name_is_not_null_or_empty()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                Name = Guid.NewGuid().ToString()
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_name_length_is_greater_than_100()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                Name = string.Join(string.Empty, Enumerable.Repeat(1,101).Select(i => i.ToString()))
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_name_length_is_less_than_or_equals_to_100()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                Name = string.Join(string.Empty, Enumerable.Repeat(1, 100).Select(i => i.ToString()))
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_CruiserWeightInGrams_is_not_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                CruiserweightInGrams = 0
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.CruiserweightInGrams);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_CruiserWeightInGrams_is_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                CruiserweightInGrams = new Random().Next(1, int.MaxValue)
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.CruiserweightInGrams);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_HeavyweightInGrams_is_not_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                HeavyweightInGrams = 0
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.HeavyweightInGrams);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_HeavyweightInGrams_is_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                HeavyweightInGrams = new Random().Next(1, int.MaxValue)
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.HeavyweightInGrams);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_LightweightInGrams_is_not_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                LightweightInGrams = 0
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.LightweightInGrams);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_LightweightInGrams_is_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                LightweightInGrams = new Random().Next(1, int.MaxValue)
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.LightweightInGrams);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_MiddleweightInGrams_is_not_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                MiddleweightInGrams = 0
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.MiddleweightInGrams);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_MiddleweightInGrams_is_greater_than_zero()
+        {
+            var model = new Models.DroneModel.Commands.CreateDroneModelCommand.CreateDroneModelCommand()
+            {
+                MiddleweightInGrams = new Random().Next(1, int.MaxValue)
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.MiddleweightInGrams);
         }
     }
 }
--- DronesWebApi.UnitTests/Validators/DroneModel/Queries/GetDroneModelQuery/GetDroneModelQueryValidatorTester.cs
+++ DronesWebApi.UnitTests/Validators/DroneModel/Queries/GetDroneModelQuery/GetDroneModelQueryValidatorTester.cs
@@ -1,20 +1,54 @@
 ﻿using DronesWebApi.Core;
 using FluentValidation;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+using DronesWebApi.Models.DroneModel.Queries.GetDroneModelQuery;
+using FluentValidation.TestHelper;
+using Xunit;
 
 namespace DronesWebApi.UnitTests.Validators.DroneModel.Queries.GetDroneModelQuery
 {
     public class GetDroneModelQueryValidatorTester
     {
-        public GetDroneModelQueryValidatorTester(IUnitOfWork unitOfWork)
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        private readonly GetDroneModelQueryValidator _validator;
+
+        public GetDroneModelQueryValidatorTester()
         {
-            //RuleFor(getDroneModelQuery => getDroneModelQuery.ModelId)
-            //    .Must(modelId =>
-            //    {
-            //        var droneModel = unitOfWork.DroneModels.Get(modelId);
+            _unitOfWork = new Mock<IUnitOfWork>();
 
-            //        return droneModel != null;
+            _validator = new GetDroneModelQueryValidator(_unitOfWork.Object);
+        }
 
-            //    }).WithMessage(getDroneModelQuery => $"Drone model with id: '{getDroneModelQuery.ModelId}' was not found");
+        [Fact]
+        public void Should_not_throw_error_when_drone_exists_in_database()
+        {
+            var model = new Models.DroneModel.Queries.GetDroneModelQuery.GetDroneModelQuery()
+            {
+                ModelId = 1
+            };
+
+            _unitOfWork.Setup(x => x.DroneModels.Get(It.IsAny<object[]>())).Returns(new Core.Domain.DroneModel());
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.ModelId);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_drone_does_not_exists_in_database()
+        {
+            var model = new Models.DroneModel.Queries.GetDroneModelQuery.GetDroneModelQuery()
+            {
+                ModelId = 1
+            };
+
+            _unitOfWork.Setup(x => x.DroneModels.Get(It.IsAny<object[]>())).Returns((Core.Domain.DroneModel)null);
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.ModelId);
         }
     }
 }

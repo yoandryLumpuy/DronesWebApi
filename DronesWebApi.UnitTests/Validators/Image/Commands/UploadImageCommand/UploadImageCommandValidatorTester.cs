89298edcaff236a9b1b5708cddb41ac6f5b2ebc0
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-07-05 15:15:55 -0400

    Adding tests for Image commands and queries validators

--- DronesWebApi.UnitTests/Validators/Image/Commands/UploadImageCommand/UploadImageCommandValidatorTester.cs
+++ DronesWebApi.UnitTests/Validators/Image/Commands/UploadImageCommand/UploadImageCommandValidatorTester.cs
@@ -1,36 +1,207 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using DronesWebApi.Commons.Configuration;
 using DronesWebApi.Core;
+using DronesWebApi.Models.Image.Commands.UploadImageCommand;
 using FluentValidation;
+using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
 
 namespace DronesWebApi.UnitTests.Validators.Image.Commands.UploadImageCommand
 {
     public class UploadImageCommandValidatorTester 
     {
-        public UploadImageCommandValidatorTester(IUnitOfWork unitOfWork, IOptions<UploadFileOptions> uploadFileOptions)
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly Mock<IOptions<UploadFileOptions>> _uploadFileOptions;
+        private readonly UploadImageCommandValidator _validator;
+        private readonly Mock<IFormFile> _formFile;
+
+        public UploadImageCommandValidatorTester()
         {
-            //var uploadFileConfig = uploadFileOptions?.Value ?? new UploadFileOptions();
+            _unitOfWork = new Mock<IUnitOfWork>();
 
-            //RuleFor(x => x.MedicationCode)
-            //    .NotNull().WithMessage("MedicationCode field can't be null")
-            //    .NotEmpty().WithMessage("MedicationCode field can not be empty")
-            //    .Must(medicationCode =>
-            //    {
-            //        var medication = unitOfWork.Medications.Get(medicationCode);
+            _uploadFileOptions = new Mock<IOptions<UploadFileOptions>>();
 
-            //        return medication != null;
+            _uploadFileOptions.Setup(x => x.Value).Returns(new UploadFileOptions());
 
-            //    }).WithMessage(command => $"Medication item with code: '{command.MedicationCode}' does not exists");
+            _formFile = new Mock<IFormFile>();
 
+            _validator = new UploadImageCommandValidator(_unitOfWork.Object, _uploadFileOptions.Object)
+                {
+                    ClassLevelCascadeMode = CascadeMode.Stop,
+                    RuleLevelCascadeMode = CascadeMode.Stop
+                };
+        }
 
-            //RuleFor(x => x.File)
-            //    .NotNull().WithMessage("File field can't be null")
-            //    .Must(file => uploadFileConfig.AllowedFormats.Contains(Path.GetExtension(file.FileName)))
-            //    .WithMessage($"Allowed image types are: '{string.Join(",", uploadFileConfig.AllowedFormats)}'")
+        [Fact]
+        public void Should_throw_error_when_MedicationCode_is_null()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
 
-            //    .Must(file => file.Length <= uploadFileConfig.MaxFileSizeInBytes)
-            //    .WithMessage($"Maximum file size is set to: {uploadFileConfig.MaxFileSizeInBytes} bytes");
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = null
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_MedicationCode_is_empty()
+        {
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "  "
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_MedicationCode_is_not_empty_nor_null()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a"
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_Medication_does_not_exists_in_database()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns((Core.Domain.Medication)null);
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a"
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_Medication_exists()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a"
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_File_is_null()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a",
+                File = null
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.File);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_FileName_is_not_included_in_allowed_formats()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            _formFile.Setup(x => x.FileName).Returns("FileName.ext");
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a",
+                File = _formFile.Object
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.File);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_FileName_is_included_in_allowed_formats()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            _formFile.Setup(x => x.FileName)
+                .Returns($"FileName{_uploadFileOptions.Object.Value.AllowedFormats[new Random().Next(0, _uploadFileOptions.Object.Value.AllowedFormats.Count)]}");
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a",
+                File = _formFile.Object
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.File);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_File_length_is_greater_than_MaxFileSizeInBytes()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            _formFile.Setup(x => x.FileName)
+                .Returns($"FileName{_uploadFileOptions.Object.Value.AllowedFormats[new Random().Next(0, _uploadFileOptions.Object.Value.AllowedFormats.Count)]}");
+
+            _formFile.Setup(x => x.Length).Returns(_uploadFileOptions.Object.Value.MaxFileSizeInBytes + 1);
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a",
+                File = _formFile.Object
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.File);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_File_length_is_less_or_equals_to_MaxFileSizeInBytes()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            _formFile.Setup(x => x.FileName)
+                .Returns($"FileName{_uploadFileOptions.Object.Value.AllowedFormats[new Random().Next(0, _uploadFileOptions.Object.Value.AllowedFormats.Count)]}");
+
+            _formFile.Setup(x => x.Length).Returns(new Random().Next(1, _uploadFileOptions.Object.Value.MaxFileSizeInBytes + 1));
+
+            var model = new Models.Image.Commands.UploadImageCommand.UploadImageCommand()
+            {
+                MedicationCode = "a",
+                File = _formFile.Object
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.File);
         }
     }
 }
--- DronesWebApi.UnitTests/Validators/Image/Queries/DownloadImageQuery/DownloadImageQueryValidatorTester.cs
+++ DronesWebApi.UnitTests/Validators/Image/Queries/DownloadImageQuery/DownloadImageQueryValidatorTester.cs
@@ -1,27 +1,75 @@
 ﻿using DronesWebApi.Core;
+using DronesWebApi.Models.Image.Queries.DownloadImageQuery;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Moq;
+using Xunit;
 
 namespace DronesWebApi.UnitTests.Validators.Image.Queries.DownloadImageQuery
 {
     public class DownloadImageQueryValidatorTester
     {
-        public DownloadImageQueryValidatorTester(IUnitOfWork unitOfWork)
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly DownloadImageQueryValidator _validator;
+
+        public DownloadImageQueryValidatorTester()
         {
-            //RuleFor(query => query.MedicationCode)
-            //    .Must(medicationCode =>
-            //    {
-            //        var medication = unitOfWork.Medications.Get(medicationCode);
+            _unitOfWork = new Mock<IUnitOfWork>();
 
-            //        return medication != null;
+            _validator = new DownloadImageQueryValidator(_unitOfWork.Object)
+            {
+                RuleLevelCascadeMode = CascadeMode.Stop,
+                ClassLevelCascadeMode = CascadeMode.Stop
+            };
+        }
 
-            //    }).WithMessage(command => $"Medication with code: '{command.MedicationCode}' does not exists")
+        [Fact]
+        public void Should_throw_error_when_Medication_does_not_exists()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns((Core.Domain.Medication)null);
 
-            //    .Must(medicationCode =>
-            //    {
-            //        var image = unitOfWork.Images.GetImageOfMedication(medicationCode);
+            var model = new Models.Image.Queries.DownloadImageQuery.DownloadImageQuery()
+            {
+                MedicationCode = "AAA1"
+            };
 
-            //        return image != null;
+            var result = _validator.TestValidate(model);
 
-            //    }).WithMessage(command => $"Medication with code: '{command.MedicationCode}' does not contains any image");
+            result.ShouldHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_throw_error_when_ImageForMedication_does_not_exists()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            _unitOfWork.Setup(x => x.Images.GetImageOfMedication(It.IsAny<string>())).Returns((Core.Domain.Image)null);
+
+            var model = new Models.Image.Queries.DownloadImageQuery.DownloadImageQuery()
+            {
+                MedicationCode = "AAA1"
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.MedicationCode);
+        }
+
+        [Fact]
+        public void Should_not_throw_error_when_Medication_and_ImageOfMedication_exists()
+        {
+            _unitOfWork.Setup(x => x.Medications.Get(It.IsAny<object[]>())).Returns(new Core.Domain.Medication());
+
+            _unitOfWork.Setup(x => x.Images.GetImageOfMedication(It.IsAny<string>())).Returns(new Core.Domain.Image());
+
+            var model = new Models.Image.Queries.DownloadImageQuery.DownloadImageQuery()
+            {
+                MedicationCode = "AAA1"
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.MedicationCode);
         }
     }
 }
